7ca0eda31ddd91380be9b065f01592c7053c864f
Chris Santero <chris@solveitsimply.com>
2015-11-01 16:26:57 -0500

    add SqlTestExecutor

---
+++ Tsqlt/SqlTestExecutor.cs
@@ -0,0 +1,25 @@
+﻿using System.Data;
+using System.Data.SqlClient;
+
+namespace Tsqlt
+{
+    public class SqlTestExecutor : ISqlTestExecutor
+    {
+        public void RunTest(SqlConnection sqlConnection, string testName)
+        {
+            var sql = GetTestExecutionCommandSql(testName);
+
+            using (var sqlCommand = new SqlCommand(sql, sqlConnection))
+            {
+                if (sqlConnection.State != ConnectionState.Open)
+                    sqlConnection.Open();
+                sqlCommand.ExecuteNonQuery();
+            }
+        }
+
+        protected virtual string GetTestExecutionCommandSql(string testName)
+        {
+            return $"exec tSQLt.Run '{testName}'";
+        }
+    }
+}
