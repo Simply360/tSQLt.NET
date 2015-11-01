82ec8f2be0aab8e6ac8bcc503676b6d40d06ac44
Chris Santero <chris@solveitsimply.com>
2015-11-01 17:44:20 -0500

    Add ITestEnvironment

---
+++ Tsqlt/BootstrappedTestEnvironment.cs
@@ -0,0 +1,42 @@
+﻿using System;
+using System.Data.SqlClient;
+
+namespace Tsqlt
+{
+    public class BootstrappedTestEnvironment : ITestEnvironment
+    {
+        private readonly string _connectionString;
+        private readonly ISqlTestExecutor _testExecutor;
+        private readonly ITestEnvironmentBootstrapper _bootstrapper;
+        private readonly object _bootstrapLock;
+        private bool IsBootstrapped { get; set; }
+
+        public BootstrappedTestEnvironment(string connectionString, ITestEnvironmentBootstrapper bootstrapper, ISqlTestExecutor testExecutor)
+        {
+            if (connectionString == null) throw new ArgumentNullException(nameof(connectionString));
+            if (bootstrapper == null) throw new ArgumentNullException(nameof(bootstrapper));
+            if (testExecutor == null) throw new ArgumentNullException(nameof(testExecutor));
+            _connectionString = connectionString;
+            _testExecutor = testExecutor;
+            _bootstrapper = bootstrapper;
+            _bootstrapLock = new object();
+        }
+
+        public void RunTest(string testName)
+        {
+            lock (_bootstrapLock)
+            {
+                if (!IsBootstrapped)
+                {
+                    _bootstrapper.BootstrapEnvironment(_connectionString);
+                    IsBootstrapped = true;
+                }
+            }
+
+            using (var connection = new SqlConnection(_connectionString))
+            {
+                _testExecutor.RunTest(connection, testName);
+            }
+        }
+    }
+}
---
+++ Tsqlt/ITestEnvironment.cs
@@ -0,0 +1,11 @@
+﻿namespace Tsqlt
+{
+    public interface ITestEnvironment
+    {
+        /// <summary>
+        /// Runs the test with the given name in the environment
+        /// </summary>
+        /// <param name="testName">The name of the test</param>
+        void RunTest(string testName);
+    }
+}
