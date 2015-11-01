edd575be279e017333cadd9e17d8fb2b138c7cb8
Chris Santero <chris@solveitsimply.com>
2015-11-01 17:43:51 -0500

    add ignored test for SqlTestExecutor

---
+++ Tsqlt.Tests/SqlTestExecutorTests.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Data.SqlClient;
+using FluentAssertions;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+
+namespace Tsqlt.Tests
+{
+    [TestClass]
+    [Ignore]
+    public class SqlTestExecutorTests
+    {
+        [TestMethod]
+        public void Asserts_when_test_does_not_exist()
+        {
+            // Arrange
+            var sqlConnectionWrapper = TestUtils.GetTestDbConnection();
+            var sut = new SqlTestExecutor();
+
+            // Act
+            Action action = () =>
+            {
+                sut.RunTest(sqlConnectionWrapper, "foo.bar");
+            };
+            
+            // Assert
+            action.ShouldThrow<InvalidOperationException>().WithMessage("The test \"foo.bar\" does not exist.");
+        }
+    }
+}
---
+++ Tsqlt.Tests/TestUtils.cs
@@ -0,0 +1,15 @@
+﻿using System.Data.SqlClient;
+
+namespace Tsqlt.Tests
+{
+    static class TestUtils
+    {
+        private const string ConnectionString =
+            @"Data Source=(localdb)\MSSQLLocalDB;Initial Catalog=Tsqlt.Tests;Integrated Security=True";
+
+        public static SqlConnection GetTestDbConnection()
+        {
+            return new SqlConnection(ConnectionString);
+        }
+    }
+}
