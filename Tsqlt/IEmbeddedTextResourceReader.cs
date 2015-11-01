963656264edbe6949e597adba4605ddd59beb1d9
Chris Santero <chris@solveitsimply.com>
2015-11-01 16:04:31 -0500

    add embedded text resource reader

---
+++ Tsqlt/IEmbeddedTextResourceReader.cs
@@ -0,0 +1,9 @@
+﻿using System.Reflection;
+
+namespace Tsqlt
+{
+    public interface IEmbeddedTextResourceReader
+    {
+        string GetResourceContents(Assembly assembly, string resourcePath);
+    }
+}
