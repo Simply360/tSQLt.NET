6dfcf45ecdfffc8a969006feed0622c5677824c5
Chris Santero <chris@solveitsimply.com>
2015-11-01 16:33:17 -0500

    add MessageWritierSqlTestExecutor

---
+++ Tsqlt.Tests/MessageWritingSqlTestExecutorTests.cs
@@ -0,0 +1,28 @@
+﻿using System.Data.SqlClient;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+using Moq;
+
+namespace Tsqlt.Tests
+{
+    [TestClass]
+    public class MessageWritingSqlTestExecutorTests
+    {
+        [TestMethod]
+        [Ignore]
+        public void Runs_inner_executor_method()
+        {
+            // Arrange
+            const string testName = "foo.bar";
+            var sqlConnection = new SqlConnection();
+            var innerExecutor = new Mock<ISqlTestExecutor>(MockBehavior.Strict);
+            var testOutputMessageWriter = new Mock<ITestOutputMessageWriter>(MockBehavior.Strict);
+            var sut = new MessageWritingSqlTestExecutor(testOutputMessageWriter.Object, innerExecutor.Object);
+
+            // Act
+            sut.RunTest(sqlConnection, testName);
+
+            // Assert
+            Assert.Fail(); // TODO
+        }
+    }
+}
