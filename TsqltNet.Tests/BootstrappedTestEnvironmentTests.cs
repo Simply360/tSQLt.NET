6d26438decc115c3b8f54a1bfbb30fedef46016c
Chris Santero <chris@solveitsimply.com>
2015-12-19 17:56:02 -0500

    add test for BootstrappedTestEnvironment

---
+++ TsqltNet.Tests/BootstrappedTestEnvironmentTests.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using FluentAssertions;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+using Moq;
+
+namespace TsqltNet.Tests
+{
+    [TestClass]
+    public class BootstrappedTestEnvironmentTests
+    {
+        [TestMethod]
+        public void GetTestRunner_calls_bootstrapper_BootstrapEnvironment()
+        {
+            // Arrange
+            var connectionString = "SOME_CONNECTION_STRING";
+            var bootstrapper = new Mock<ITestEnvironmentBootstrapper>(MockBehavior.Strict);
+            bootstrapper.Setup(b => b.BootstrapEnvironment()).Returns(connectionString).Verifiable();
+
+            Func<string, ITestRunner> testRunnerFactory = actualConnectionString =>
+            {
+                actualConnectionString.Should().Be(connectionString);
+                return new Mock<ITestRunner>().Object;
+            };
+            var sut = new BootstrappedTestEnvironment(bootstrapper.Object, testRunnerFactory);
+
+            // Act
+            sut.GetTestRunner();
+            sut.GetTestRunner();
+
+            // Assert
+            bootstrapper.Verify(b => b.BootstrapEnvironment(), Times.Once);
+        }
+    }
+}
